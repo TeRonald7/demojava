888fad87b7874cf88b157156889f3d4e66088f5c
tom_pro <goodmorning@qq.com>
2022-03-07 14:46:16 +0800

    3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args){
+        //ftftftft
+        /*ggffgg
+        jij
+         */
         System.out.println("Hello New World");
-
     }
 }
